9a733abaec850954bbe5b0c4ca70837cac9604e3
James Holden <james_holden@londonmarketing.com>
2017-11-08 15:52:07 +0000

    changes

--- website/runUpdates.sh
+++ website/runUpdates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+docker exec -it $1 mv /var/www/html/envprod /var/www/html/.env
 docker exec -it $1 chmod -R 777 /var/www/html/storage
 docker exec -it $1 chmod -R 777 /var/www/html/bootstrap/cache
 docker exec -it $1 php artisan key:generate
