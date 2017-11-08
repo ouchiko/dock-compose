68bbab8e5882288737b6eefe900b852dcca5aa80
James Holden <james_holden@londonmarketing.com>
2017-11-08 16:05:07 +0000

    changes

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ docker-compose build screenshot
 docker-compose run composer
 docker-compose up -d
 docker ps
+
+
+sleep 10
+
+docker exec -it docker_website_1 mv /var/www/html/envprod /var/www/html/.env
+docker exec -it docker_website_1 chmod -R 777 /var/www/html/storage
+docker exec -it docker_website_1 chmod -R 777 /var/www/html/bootstrap/cache
+docker exec -it docker_website_1 php artisan key:generate
+docker exec -it docker_website_1 php artisan config:clear
+docker exec -it docker_website_1 php artisan config:cache
