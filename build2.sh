c0cb55feef12c681663ca8709018d83c77180406
James Holden <james_holden@londonmarketing.com>
2017-11-08 16:11:23 +0000

    Test

--- build2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker exec -it $1 mv /var/www/html/envprod /var/www/html/.env
-docker exec -it $1 chmod -R 777 /var/www/html/storage
-docker exec -it $1 chmod -R 777 /var/www/html/bootstrap/cache
-docker exec -it $1 php artisan key:generate
-docker exec -it $1 php artisan config:clear
-docker exec -it $1 php artisan config:cache
